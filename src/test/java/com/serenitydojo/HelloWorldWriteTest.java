e17f786d0748453aa419abd8a428a792617defa8
Denver Lecky <97166642+dlecky9@users.noreply.github.com>
2024-01-31 16:43:35 +0000

    Excercise 1

--- src/test/java/com/serenitydojo/HelloWorldWriteTest.java
+++ src/test/java/com/serenitydojo/HelloWorldWriteTest.java
@@ -2,10 +2,28 @@ package com.serenitydojo;
 
 import org.junit.Test;
 
+import java.util.Locale;
+
 public class HelloWorldWriteTest {
     @Test
     public void shouldWriteHelloWorldToTheConsole(){
         HelloWorldWriter writer = new HelloWorldWriter();
         writer.writeHelloWorld();
+    }
+
+    @Test
+    public void declaringNumericVariable(){
+        final int ageThisYear = 42;
+        int ageNextYear = ageThisYear + 1;
+        System.out.println("age = " + ageNextYear);
+    }
+
+    @Test
+    public void workingWithStrings(){
+        String firstName = "Sarah-Jane";
+        String upperCaseFirstName = firstName.toUpperCase(Locale.ROOT);
+        String replaceFirstName = firstName.replace("Sarah", "Mary");
+        System.out.println(upperCaseFirstName);
+        System.out.println(replaceFirstName);
     }
 }
